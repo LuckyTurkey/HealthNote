34f8f7d4c5c1e1e790147b0f1985aa10aac0e822
Yeji <mnbvcxzyj@gmail.com>
2024-05-13 19:25:48 +0900

    commit

---
+++ src/main/java/com/sosigae/LuckeyTurkey/LuckeyTurkeyApplication.java
@@ -0,0 +1,13 @@
+package com.sosigae.LuckeyTurkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckeyTurkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckeyTurkeyApplication.class, args);
+	}
+
+}
