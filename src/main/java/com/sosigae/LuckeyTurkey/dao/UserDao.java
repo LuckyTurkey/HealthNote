99c42a026764a6b1f3b09321c8c7d257c83ff407
hyebim <shb010424@gmail.com>
2024-05-23 21:41:45 +0900

    /

--- src/main/java/com/sosigae/LuckeyTurkey/dao/UserDao.java
+++ src/main/java/com/sosigae/LuckeyTurkey/dao/UserDao.java
@@ -2,6 +2,7 @@
 package com.sosigae.LuckeyTurkey.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.sosigae.LuckeyTurkey.domain.User;
@@ -10,7 +11,7 @@ import com.sosigae.LuckeyTurkey.domain.User;
 @Mapper
 public interface UserDao {
 
-    User loginMember(String user_id, String password);
+	User loginMember(@Param("user_id") String user_id, @Param("password") String password);
 
     void registerMember(User user);
 
