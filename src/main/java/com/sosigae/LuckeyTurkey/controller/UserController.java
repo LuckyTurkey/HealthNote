ddb0e908bac717cff6ac9c57a636c59a3f9b6a58
hyebim <shb010424@gmail.com>
2024-05-27 03:03:12 +0900

    로그인, 회원가입

--- src/main/java/com/sosigae/LuckeyTurkey/controller/UserController.java
+++ src/main/java/com/sosigae/LuckeyTurkey/controller/UserController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sosigae.LuckeyTurkey.domain.User;
 import com.sosigae.LuckeyTurkey.service.UserService;
- 
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -21,14 +22,19 @@ public class UserController {
     @GetMapping("/register")
     public String showRegisterForm(Model model) {
         model.addAttribute("user", new User());
-        return "/user/register";
+        return "user/register";
     }
-    
+
     @PostMapping("/register")
-    public String registerUser(@ModelAttribute User user, Model model) {
-        userService.registerMember(user);
-        model.addAttribute("message", "회원가입이 완료되었습니다.");
-        return "redirect:/user/login"; // 성공 시 로그인 페이지로 이동
+    public String registerUser(@ModelAttribute User user, RedirectAttributes redirectAttributes) {
+    	userService.registerMember(user);
+        redirectAttributes.addFlashAttribute("message", "회원가입이 완료되었습니다.");
+        return "user/login"; // 성공 시 로그인 페이지로 이동
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "user/login";
     }
 
     @PostMapping("/login")
@@ -39,12 +45,7 @@ public class UserController {
             return "/search/search"; // 성공 시 메인 페이지로 이동 (잠시 search 페이지로 수정 !!!)
         } catch (IllegalArgumentException e) {
             model.addAttribute("loginResult", "유효하지 않은 사용자입니다.");
-            return "/user/login"; // 실패 시 다시 로그인 페이지로 이동
+            return "user/login"; // 실패 시 다시 로그인 페이지로 이동
         }
-    }
-    
-    @GetMapping("/login")
-    public String showLoginForm() {
-        return "/user/login"; // 로그인 페이지로 이동
     }
 }
