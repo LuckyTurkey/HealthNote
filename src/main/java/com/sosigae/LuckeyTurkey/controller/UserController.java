90bf9bbaf9ac99730ae4eadb1bd4a539116fa017
hyebim <shb010424@gmail.com>
2024-05-26 16:01:56 +0900

    .

--- src/main/java/com/sosigae/LuckeyTurkey/controller/UserController.java
+++ src/main/java/com/sosigae/LuckeyTurkey/controller/UserController.java
@@ -3,6 +3,7 @@ package com.sosigae.LuckeyTurkey.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +35,10 @@ public class UserController {
             model.addAttribute("loginResult", "유효하지 않은 사용자입니다.");
             return "login"; // 실패 시 다시 로그인 페이지로 이동
         }
+    }
+    
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login"; // 로그인 페이지로 이동
     }
 }
