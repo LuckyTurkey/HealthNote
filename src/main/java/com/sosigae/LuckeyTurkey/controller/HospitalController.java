2dc89648d308447a850d35d7ce90406964748fc6
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2024-06-21 06:20:20 +0900

    Feat: 병원 스케줄 조회

--- src/main/java/com/sosigae/LuckeyTurkey/controller/HospitalController.java
+++ src/main/java/com/sosigae/LuckeyTurkey/controller/HospitalController.java
@@ -2,20 +2,27 @@ package com.sosigae.LuckeyTurkey.controller;
 
 import com.sosigae.LuckeyTurkey.domain.Doctor;
 import com.sosigae.LuckeyTurkey.domain.Hospital;
+import com.sosigae.LuckeyTurkey.domain.Reservation;
 import com.sosigae.LuckeyTurkey.service.HospitalService;
+import com.sosigae.LuckeyTurkey.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
 public class HospitalController {
     @Autowired
     private HospitalService hospitalService;
+
+    @Autowired
+    private ReservationService reservationService;
 
     // 병원 상세 조회
     @GetMapping("/hospital/{hospitalId}")
@@ -30,5 +37,22 @@ public class HospitalController {
     @GetMapping("/reservation/search/hospital")
     public List<Hospital> searchHospitals(@RequestParam String name) {
         return hospitalService.searchHospitalsByName(name);
+    }
+
+    // 스케줄 리스트 출력
+    @GetMapping("/hospital/{hospitalId}/reservationList")
+    public String getHospitalReservationList(@PathVariable int hospitalId, Model model) {
+        List <Reservation> reservations = reservationService.getReservationByHospitalId(hospitalId);
+        Hospital hospital = hospitalService.getHospitalInfo();
+        model.addAttribute("reservations", reservations);
+
+        return "/hospital/reservationList";
+    }
+
+    @GetMapping("/hospital/{hospitalId}/newReservation")
+    public String showReservationForm(@PathVariable int hospitalId, Model model) {
+        List <Reservation> reservations = reservationService.getReservationByHospitalId(hospitalId);
+        model.addAttribute("reservations", reservations);
+        return "/hospital/reservationList";
     }
 }
--- src/main/java/com/sosigae/LuckeyTurkey/service/HospitalService.java
+++ src/main/java/com/sosigae/LuckeyTurkey/service/HospitalService.java
@@ -4,6 +4,7 @@ import com.sosigae.LuckeyTurkey.dao.mybatis.mapper.DoctorMapper;
 import com.sosigae.LuckeyTurkey.dao.mybatis.mapper.HospitalMapper;
 import com.sosigae.LuckeyTurkey.domain.Doctor;
 import com.sosigae.LuckeyTurkey.domain.Hospital;
+import com.sosigae.LuckeyTurkey.repository.HospitalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,8 @@ public class HospitalService {
     private HospitalMapper hospitalMapper;
     @Autowired
     private DoctorMapper doctorMapper;
+    @Autowired
+    private HospitalRepository hospitalRepository;
 
     // 병원 검색
     public List<Hospital> searchHospitals(String name, String department) {
@@ -53,5 +56,9 @@ public class HospitalService {
 
     public List<Doctor> findDoctorsByDoctorName(String doctorName) {
         return doctorMapper.findDoctorsByDoctorName(doctorName);
+    }
+
+    public Hospital getHospitalById(String id) {
+        return hospitalRepository.findById(id);
     }
 }
