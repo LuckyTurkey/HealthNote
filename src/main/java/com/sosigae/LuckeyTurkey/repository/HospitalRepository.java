2afac5038db78d5e08533d04ad39af64d50ad786
da-jeong <loveda0529@naver.com>
2024-06-23 00:00:34 +0900

    [Fix] 회원가입 hospitalId 수정

--- src/main/java/com/sosigae/LuckeyTurkey/repository/HospitalRepository.java
+++ src/main/java/com/sosigae/LuckeyTurkey/repository/HospitalRepository.java
@@ -1,7 +1,9 @@
 package com.sosigae.LuckeyTurkey.repository;
 
 import com.sosigae.LuckeyTurkey.domain.Hospital;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -9,7 +11,8 @@ import org.springframework.stereotype.Repository;
 public interface HospitalRepository extends JpaRepository<Hospital, Integer>  {
 
 	Hospital findByIdAndPassword(String id, String password);
-    //Hospital findById(String id);
-    
-    //Hospital findByUserId(int userId);
+
+	@Query("SELECT h FROM Hospital h WHERE h.id = :loginId")
+	Hospital findHospitalByLoginId(@Param("loginId") String loginId);
+
 }
