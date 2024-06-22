dfe07e739445ec20f37f8b82ada73b07736426b4
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2024-06-23 02:19:18 +0900

    Fix: 로그인 후 redirect 주소

--- src/main/java/com/sosigae/LuckeyTurkey/domain/Hospital.java
+++ src/main/java/com/sosigae/LuckeyTurkey/domain/Hospital.java
@@ -39,6 +39,9 @@ public class Hospital implements Serializable {
     private String satCloseTime;
     private String email;
     private String phone;
+    @Column(name = "IS_ADMIN")
+    private int isAdmin;
+
 
     // 진료 과 추가
     private String department;
